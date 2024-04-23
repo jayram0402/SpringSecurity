9fd2e3275487ea1866dcad1b95f285f6c196609d
Jayram Sharma <147648366+jayram0402@users.noreply.github.com>
2024-04-23 12:10:50 +0530

    SpringJwtApplicationTests.java

---
+++ src/test/java/com/helloIftekhar/springJwt/SpringJwtApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.helloIftekhar.springJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
